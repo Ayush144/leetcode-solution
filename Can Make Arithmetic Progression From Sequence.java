20117b4fecf4dcfeee6bdb4a4b064ece402c28b4
Ayush Tiwari <55916664+Ayush144@users.noreply.github.com>
2022-08-08 22:40:21 +0530

    Update Can Make Arithmetic Progression From Sequence.java

--- Can Make Arithmetic Progression From Sequence.java
+++ Can Make Arithmetic Progression From Sequence.java
@@ -3,7 +3,7 @@ A sequence of numbers is called an arithmetic progression if the difference betw
 Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
 
  
-
+testcases
 Example 1:
 
 Input: arr = [3,5,1]
